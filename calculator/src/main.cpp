f0bf47c71698b4fd0e76eae4c063250389155279
Moises Cintra <moisescintra@gee.inatel.br>
2026-07-01 20:09:54 -0300

    fix: fixing lint main.cpp

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -4,21 +4,21 @@
 
 #include "calculator.hpp"
 
-int main()
-{
-    double number1 = 0.0;
-    double number2 = 0.0;
-    std::cout << "Hello Calculator" << std::endl;
-    std::cout << "Enter Number 1: ";
-    std::cin >> number1;
-    std::cout << "Enter Number 2: ";
-    std::cin >> number2;
+int main() {
+  double number1 = 0.0;
+  double number2 = 0.0;
+  std::cout << "Hello Calculator" << std::endl;
+  std::cout << "Enter Number 1: ";
+  std::cin >> number1;
+  std::cout << "Enter Number 2: ";
+  std::cin >> number2;
 
-    Calculator<double> c (number1, number2);
-    
-    std::cout << "Result -> " << number1 << " / " << number2 << " = " << c.divide() << std::endl;
-    
-    return 0;
+  Calculator<double> c(number1, number2);
+
+  std::cout << "Result -> " << number1 << " / " << number2 << " = "
+            << c.divide() << std::endl;
+
+  return 0;
 }
 
 #endif
